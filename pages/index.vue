35d5308a8ea56b828898f02781d2438590e383ce
franciscovasquezv <franjavi4111@gmail.com>
2024-01-29 21:04:43 -0500

    initial setup for vuex, plugins and axios

---
+++ pages/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h1>HOLA SOY H1</h1>
+     <h1>{{$count}}</h1> 
+     <button @click="increment">Incrementar</button>
+ 
+   
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import { example } from '@/store'
+export default Vue.extend({
+
+  computed: {
+    $count() {
+      return example.$count
+    }
+  },
+
+  methods: {
+    increment() {
+      example.increment(1)
+    }
+  }
+
+})
+</script>
